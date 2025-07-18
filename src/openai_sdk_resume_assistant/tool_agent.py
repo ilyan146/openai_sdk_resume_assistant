c3b64eab90caab1394cc9506b44890fa3a6581d6
ilyan <ilyan146@gmail.com>
2025-07-18 15:33:35 +0400

    WIP; making use of AsyncExitStack to make server client creation and connection

--- src/openai_sdk_resume_assistant/tool_agent.py
+++ src/openai_sdk_resume_assistant/tool_agent.py
@@ -1,4 +1,5 @@
 import os
+from contextlib import AsyncExitStack, asynccontextmanager
 
 from agents import Agent
 from agents.mcp import MCPServerStdio
@@ -18,18 +19,23 @@ trying different options and sites are needed.
 playwright_params = {"command": "npx", "args": ["@playwright/mcp@latest"]}
 file_storage_path = os.path.abspath(os.path.join(os.getcwd(), "file_storage"))
 files_params = {"command": "npx", "args": ["-y", "@modelcontextprotocol/server-filesystem", file_storage_path]}
-tools_params = {"command": "uv", "args": ["run", "src/openai_sdk_resume_assistant/agent_tools.py"]}
+tools_params = {"command": "uv", "args": ["run", "agent_tools.py"]}
 
 
 # Create mcp servers with the params
 params_list = [playwright_params, files_params]
-tool_mcp_servers = [MCPServerStdio(params, client_session_timeout_seconds=30) for params in params_list]
 
 
-# Connect to the servers
-async def connect_to_servers(mcp_servers):
-    for server in mcp_servers:
-        await server.connect()
+@asynccontextmanager
+async def get_mcp_servers():
+    async with AsyncExitStack() as stack:
+        tool_mcp_servers = [
+            await stack.enter_async_context(MCPServerStdio(params=params, client_session_timeout_seconds=60))
+            for params in params_list
+        ]
+    yield tool_mcp_servers
 
 
-tool_agent = Agent(name="ToolAgent", instructions=TOOL_AGENT_INSTRUCTIONS, model="gpt-4o", mcp_servers=tool_mcp_servers)
+async def get_tool_agent(mcp_servers_list):
+    tool_agent = Agent(name="ToolAgent", instructions=TOOL_AGENT_INSTRUCTIONS, model="gpt-4o", mcp_servers=mcp_servers_list)
+    return tool_agent
