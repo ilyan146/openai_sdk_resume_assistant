05a6d91a0ce27a43857fc88b73ef71aaa93da102
Nazer, Mohamed Ilyan <ilyan146@gmail.com>
2025-08-15 15:51:18 +0800

    WIP; adding rag agent server file with some todos for the functionalities for the rag agent

---
+++ src/openai_sdk_resume_assistant/mcp_servers/rag_agent_server.py
@@ -0,0 +1,8 @@
+"""
+This file will be used to serve a RAG agent as a tool for the resume agent.
+
+TODO:
+- RAG tool --> Use embedding models to create vectorstore and use it to retrieve relevant information
+- RAG Agent with tool integration
+- RAG Agent is provided to other agents through MCP servers
+"""
