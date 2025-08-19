3c7577a9fee7fa8814709b4bf02458a7d4372c0b
Nazer, Mohamed Ilyan <ilyan146@gmail.com>
2025-08-19 14:42:17 +0800

    WIP; start creating RAGTool

--- src/openai_sdk_resume_assistant/mcp_servers/rag_agent_server.py
+++ src/openai_sdk_resume_assistant/mcp_servers/rag_agent_server.py
@@ -6,3 +6,15 @@ TODO:
 - RAG Agent with tool integration
 - RAG Agent is provided to other agents through MCP servers
 """
+
+
+class RAGTool:
+    """
+    RAGTool class for connecting and retrieving from a vectorstore.
+    """
+
+    def __init__(self, collection, azure_ai_client):
+        """
+        Initialize RAGtool with collection and azure ai client
+        connection.
+        """
