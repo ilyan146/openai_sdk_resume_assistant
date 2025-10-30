06b0917220c6aa3a4f387fba63a921dece890629
Nazer, Mohamed Ilyan <ilyan146@gmail.com>
2025-10-30 18:36:12 +0400

    Working backend using docker compose and seperate backend dir

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,4 +28,4 @@ def health():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000)
