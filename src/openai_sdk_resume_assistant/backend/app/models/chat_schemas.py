ecceeea8eb274df5b94f902122578cc81779e969
Nazer, Mohamed Ilyan <ilyan146@gmail.com>
2025-11-09 00:46:22 +0400

    WIP; Add new dependencies for using mongodb and add data_access_layer module

--- src/openai_sdk_resume_assistant/backend/app/models/chat_schemas.py
+++ src/openai_sdk_resume_assistant/backend/app/models/chat_schemas.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel
+from datetime import datetime
+from uuid import uuid4
+
+from pydantic import BaseModel, Field
 
 
 class QuestionRequest(BaseModel):
@@ -10,3 +13,24 @@ class UploadFilesResponse(BaseModel):
     text_count: int
     errors: list[str]
     success: bool
+
+
+class ChatMessage(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid4()))
+    role: str = Field(..., description="Role of the message sender, e.g., 'user' or 'agent'")
+    content: str
+    timestamp: datetime = datetime.utcnow()  # type: ignore
+
+
+class ChatMemory(BaseModel):
+    id: str
+    chat_name: str
+    chat_messages: list[ChatMessage] = Field(default_factory=list)
+
+    @staticmethod
+    def from_doc(doc) -> "ChatMemory":  # To convert back to pydantic model
+        return ChatMemory(
+            id=str(doc["_id"]),
+            chat_name=doc["chat_name"],
+            chat_messages=[ChatMessage(**msg) for msg in doc.get("chat_messages", [])],
+        )
